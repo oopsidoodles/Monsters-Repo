214117aa052b51ad0db87e2f302f5c18716c6e75
oopsidoodles <vilimilner@gmail.com>
2019-12-05 20:35:52 -0500

    Updates

---
+++ precache-manifest.011711ffb7b0d7d7814da8cb2a39112c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b8a5ddf8f0bacee83d7339c6d4b4a2",
+    "url": "/Monsters-Repo/index.html"
+  },
+  {
+    "revision": "a73b21cd7be5f0623aac",
+    "url": "/Monsters-Repo/static/css/main.52318f4c.chunk.css"
+  },
+  {
+    "revision": "e681d63e092c05d48422",
+    "url": "/Monsters-Repo/static/js/2.7699bed9.chunk.js"
+  },
+  {
+    "revision": "a73b21cd7be5f0623aac",
+    "url": "/Monsters-Repo/static/js/main.5990ee20.chunk.js"
+  },
+  {
+    "revision": "673c435a0aefea898fef",
+    "url": "/Monsters-Repo/static/js/runtime-main.e2e7b108.js"
+  }
+]);
